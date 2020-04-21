969394d4c03d5184a6298b5067ae8ed882987127
chenghao <hao.cheng@karakal.com.cn>
2020-04-21 15:41:41 +0800

    upgrade some config

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescriptPlugin from 'rollup-plugin-typescript2';
+import { eslint } from 'rollup-plugin-eslint';
 import pkg from './package.json';
 
 function forUmd() {
@@ -7,12 +8,13 @@ function forUmd() {
         name: pkg.name,
         globals: {
             react: 'React',
+            'react-dom': 'ReactDom',
         },
     };
 }
 function fromSource(format) {
     return {
-        input: 'src/index.ts',
+        input: 'src/index.tsx',
         external: ['react'],
         output: {
             ...(format === 'umd' && forUmd()),
@@ -21,12 +23,13 @@ function fromSource(format) {
             sourcemap: true,
         },
         plugins: [
+            eslint(),
             nodeResolve({
                 extensions: ['.ts', '.tsx'],
-                mainFields: ['module', 'main']
+                mainFields: ['module', 'main'],
             }),
             typescriptPlugin(),
         ],
     };
 }
-export default [...['esm', 'cjs', 'umd'].map(format => fromSource(format))];
+export default [...['esm', 'cjs', 'umd'].map((format) => fromSource(format))];
---
+++ template/src/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * File: index.tsx
+ * Desc: index
+ * File Created: 2020-04-21 15:33:37
+ * Author: chenghao
+ * ------
+ * Copyright 2020 - present, chenghao
+ */
+import React from 'react';
+
+const App = () => {
+    return <div>App</div>
+}
+
+export default App;
